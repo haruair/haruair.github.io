6a90d4701505007b1af6ef0cab06f68ce42f7e5b
Edward Kim <onward.edkim@gmail.com>
2017-01-01 16:51:45 +1100

    Update travis ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -54,7 +54,7 @@ if [ -n "$TRAVIS_BUILD_ID" ]; then
 fi
 
 REPO_NAME=$(basename $REPO)
-TARGET_DIR=$(sudo mktemp -d /tmp/$REPO_NAME.XXXX)
+TARGET_DIR=$TEMP_DIR
 REV=$(git rev-parse HEAD)
 git clone --branch ${TARGET_BRANCH} ${REPO} ${TARGET_DIR}
 rsync -rt --delete --exclude=".git" --exclude=".travis.yml" $SOURCE_DIR/ $TARGET_DIR/
